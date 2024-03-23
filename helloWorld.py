6028b4177e2ae01cecbb3f593d8a0d3363cdcc9a
MillieC <cwy.millie@gmail.com>
2024-03-23 23:37:49 +0000

    Changed variable name as required

--- helloWorld.py
+++ helloWorld.py
@@ -2,4 +2,4 @@ import datetime
 year = int(input('Please enter your year of birth:'))
 current_year = datetime.datetime.now().year
 age = current_year - year
-print("You are {} years old.".format(age))
+print("You are {} years old. Thank you!".format(age))
