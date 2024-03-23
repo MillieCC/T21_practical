f72c43512cf8b7c991871845e878beff7740b53a
MillieC <cwy.millie@gmail.com>
2024-03-23 23:41:20 +0000

    Millie has added the calculation part.

--- helloWorld.py
+++ helloWorld.py
@@ -1,5 +1,6 @@
 import datetime
 year = int(input('Please enter your year of birth:'))
+#Add calculation
 current_year = datetime.datetime.now().year
 age = current_year - year
 print("You are {} years old. Thank you!".format(age))
